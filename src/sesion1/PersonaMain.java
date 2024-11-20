cf0707ceb142ed709f3fd3c05a2538d9a653d8dd
Leonardo-Rozza <leonardo155964@gmail.com>
2024-11-19 23:30:59 -0300

    19/11/24 empezando con la OOP

---
+++ src/sesion1/PersonaMain.java
@@ -0,0 +1,11 @@
+package sesion1;
+
+public class PersonaMain {
+  public static void main(String[] args) {
+    Persona persona = new Persona();
+
+    persona.setNombre("Juan");
+
+
+  }
+}
---
+++ src/sesion2/ejercicio1/Persona.java
@@ -0,0 +1,14 @@
+package sesion2.ejercicio1;
+
+public class Persona {
+
+  //Atributos
+  private String nombre;
+  private int edad;
+
+  //Constructor Parametrizado
+  public Persona(String nombre, int edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+}
