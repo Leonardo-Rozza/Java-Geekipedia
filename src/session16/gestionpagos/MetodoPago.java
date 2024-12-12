97c1eb46e786d4edb05c9866a312192c8a0f130f
Leonardo-Rozza <leonardo155964@gmail.com>
2024-12-12 12:17:54 -0300

    Documentando la interfaz

--- src/session16/gestionpagos/MetodoPago.java
+++ src/session16/gestionpagos/MetodoPago.java
@@ -1,9 +1,20 @@
 package session16.gestionpagos;
 
+/**
+ * Interfaz que define los métodos para procesar pagos.
+ */
 public interface MetodoPago {
 
+  /**
+   *Este metodo procesa los pagos.
+   * @param monto
+   */
   void procesarPago(double monto);
 
+  /**
+   * Obtiene y retorna los detalles del pago.
+   * @return String
+   */
   String obtenerDetallesPago();
 
 }
